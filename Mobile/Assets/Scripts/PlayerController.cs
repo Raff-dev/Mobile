2e23dea0da71fd1d61fced9cac387b7e8b12c1b7
Raff <rafal.lazicki99@gmail.com>
2021-04-24 17:53:30 +0200

    implement simple movement

--- Mobile/Assets/Scripts/PlayerController.cs
+++ Mobile/Assets/Scripts/PlayerController.cs
@@ -1,18 +1,30 @@
-﻿using System.Collections;
+﻿using System.Globalization;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
+    public float velocity = 5;
+
+    private Vector3 moveDirection;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
     }
+
+    private void FixedUpdate()
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
+        rb.MovePosition(rb.position + transform.TransformDirection(moveDirection) * velocity * Time.deltaTime);
+    }
+
 }
