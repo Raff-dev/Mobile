6c55c1b6dfac2c9473c39fd92e2b2125ad55ecb5
Raff <rafal.lazicki99@gmail.com>
2021-05-21 17:41:44 +0200

    make all profile information retrieveable

--- service/profiles/serializers.py
+++ service/profiles/serializers.py
@@ -1,18 +1,22 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
-from .models import Profile, Skin
+from rest_framework.relations import StringRelatedField
+from .models import Profile, Skin, SkinOwnership
+
+
+class SkinOwnerShipSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = SkinOwnership
+        fields = ['name']
 
 
 class ProfileSerializer(serializers.ModelSerializer):
+    skins = SkinOwnerShipSerializer(many=True, read_only=True)
+
     class Meta:
         model = Profile
         exclude = ['password']
-
-
-class SkinSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Skin
-        fields = '__all__'
 
 
 class RegisterSerializer(serializers.ModelSerializer):
