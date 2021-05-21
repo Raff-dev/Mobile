696dc35634ca664d5542a47fb725a9866caaa830
Raff <rafal.lazicki99@gmail.com>
2021-05-21 17:42:11 +0200

    remove unused ViewSet

--- service/profiles/urls.py
+++ service/profiles/urls.py
@@ -1,10 +1,9 @@
 from rest_framework import routers
 
-from .views import ProfileViewSet, SkinViewSet
+from .views import ProfileViewSet
 
 router = routers.DefaultRouter()
 router.register('Profiles', ProfileViewSet, basename='Profiles')
-router.register('SkinViewSet', SkinViewSet, basename='Skins')
 
 urlpatterns = router.urls + [
 
