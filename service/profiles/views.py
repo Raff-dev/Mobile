12bd764a7cb326285cec8c311e26d7930fe19a03
Raff <rafal.lazicki99@gmail.com>
2021-05-21 17:41:58 +0200

    remove unused ViewSet

--- service/profiles/views.py
+++ service/profiles/views.py
@@ -2,17 +2,10 @@ from django.contrib.auth import get_user_model
 from rest_framework import mixins, viewsets, permissions
 
 from .permissions import IsOwnerOrReadOnly
-from .models import Skin
-from .serializers import ProfileSerializer, SkinSerializer
+from .serializers import ProfileSerializer
 
 
 class ProfileViewSet(viewsets.GenericViewSet, mixins.DestroyModelMixin, mixins.RetrieveModelMixin):
     queryset = get_user_model().objects.all()
     serializer_class = ProfileSerializer
-    permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
-
-
-class SkinViewSet(viewsets.GenericViewSet, mixins.CreateModelMixin, mixins.RetrieveModelMixin):
-    queryset = Skin.objects.all()
-    serializer_class = SkinSerializer
     permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
