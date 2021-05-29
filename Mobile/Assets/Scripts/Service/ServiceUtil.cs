200030f02b22731de747aa2acabd2ea4c6fbd178
Raff <rafal.lazicki99@gmail.com>
2021-05-29 20:01:27 +0200

    implement login on app open

--- Mobile/Assets/Scripts/Service/ServiceUtil.cs
+++ Mobile/Assets/Scripts/Service/ServiceUtil.cs
@@ -10,6 +10,8 @@ public static class ServiceUtil {
     public const string AUTH_URL = BASE_URL + "/auth";
     public const string REGISTER_URL = AUTH_URL + "/users/";
     public const string LOGIN_URL = AUTH_URL + "/jwt/create/";
+    public const string REFRESH_URL = AUTH_URL + "/jwt/refresh/";
+    public const string VERIFY_URL = AUTH_URL + "/jwt/verify/";
     public const string USER_URL = AUTH_URL + "/users/me/";
 
     public const int STATUS_201_CREATED = 201;
@@ -21,6 +23,12 @@ public static class ServiceUtil {
     public const string PASSWORD_FIELD_NAME = "password";
     public const string PASSWORD2_FIELD_NAME = "re_password";
 
+    public const string REFRESH_FIELD_NAME = "refresh";
+    public const string TOKEN_FIELD_NAME = "token";
+
     public const string ERROR_SOMETING_WRONG = "Something went wrong, check your connection and try later";
     public const string ERROR_UNKNOWN = "Unknown error has occured";
+
+    public const string WARNING_LOGGED_OUT = "You are logged out";
+
 }
