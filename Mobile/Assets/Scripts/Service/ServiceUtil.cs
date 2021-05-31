de535a5d148ac224bddb47f3283c20bbc9f1ca96
Raff <rafal.lazicki99@gmail.com>
2021-05-31 22:08:52 +0200

    Update ServiceUtil.cs

--- Mobile/Assets/Scripts/Service/ServiceUtil.cs
+++ Mobile/Assets/Scripts/Service/ServiceUtil.cs
@@ -5,15 +5,19 @@ using UnityEngine;
 using UnityEngine.Networking;
 
 public static class ServiceUtil {
-    public const string BASE_URL = "http://localhost:8000";
-    public const string PROFILE_URL = BASE_URL + "/Profiles/";
-    public const string AUTH_URL = BASE_URL + "/auth";
-    public const string REGISTER_URL = AUTH_URL + "/users/";
-    public const string LOGIN_URL = AUTH_URL + "/jwt/create/";
-    public const string REFRESH_URL = AUTH_URL + "/jwt/refresh/";
-    public const string VERIFY_URL = AUTH_URL + "/jwt/verify/";
-    public const string USER_URL = AUTH_URL + "/users/me/";
-    private const string STELLAR_POINTS_URL = AUTH_URL + "/users/me/";
+    public const string BASE_URL = "http://localhost:8000/";
+    public const string AUTH_URL = BASE_URL + "auth/";
+
+    public const string REGISTER_URL = AUTH_URL + "users/";
+    public const string USER_URL = AUTH_URL + "users/me/";
+
+    public const string LOGIN_URL = AUTH_URL + "jwt/create/";
+    public const string REFRESH_URL = AUTH_URL + "jwt/refresh/";
+    public const string VERIFY_URL = AUTH_URL + "jwt/verify/";
+
+    public const string PROFILE_URL = BASE_URL + "Profiles/";
+    public const string PROFILE_INFO_URL = PROFILE_URL + "info/";
+    public const string STELLAR_POINTS_GAIN_URL = PROFILE_URL + "gain_stellar_points/";
 
     public const int STATUS_201_CREATED = 201;
     public const int STATUS_200_OK = 200;
@@ -26,10 +30,4 @@ public static class ServiceUtil {
 
     public const string REFRESH_FIELD_NAME = "refresh";
     public const string TOKEN_FIELD_NAME = "token";
-
-
-
-    public static string getStellarPointsUrl(int id) {
-        return STELLAR_POINTS_URL + id.ToString() + '/';
-    }
 }
