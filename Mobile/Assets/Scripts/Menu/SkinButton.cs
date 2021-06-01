b8156565d3ea5b7df21d389b68bf86dde38a8644
Raff <rafal.lazicki99@gmail.com>
2021-06-02 01:27:55 +0200

    add price text to SkinButton

--- Mobile/Assets/Scripts/Menu/SkinButton.cs
+++ Mobile/Assets/Scripts/Menu/SkinButton.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SkinButton : MonoBehaviour {
     public GameObject skin;
     public CustomizationManager customizationManager;
+    public TMPro.TMP_Text priceText;
 
     public void onClick() {
         customizationManager.select(skin);
